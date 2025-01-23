dea23501a20fc0b01789fa4e5c206a8c555a117f
SamuelLemos01 <samuellemos907@gmail.com>
2025-01-23 10:13:07 -0500

    Primeros pasos en la logica

---
+++ app.js
@@ -0,0 +1,3 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.//
+
+//Funciones a desarrollar: Agregar nombres, validar entrada, visualizacion de la lista y sorteo aleatorio
